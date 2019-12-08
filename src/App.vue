74b2f82a2b82527f76985984c987a192ebcd05ae
circleone <1114025502@qq.com>
2019-12-08 10:45:22 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <m-header></m-header>
+    <tab></tab>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import MHeader from 'components/m-header/m-header.vue'
+  import Tab from 'components/tab/tab'
+  export default {
+    components: {
+      MHeader,
+      Tab
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+</style>
